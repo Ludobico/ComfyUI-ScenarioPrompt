59a85040883c3528c8f8f98831bae474bbf40322
Ludobico <aqs450@gmail.com>
2024-02-19 17:14:24 +0900

    test : test

---
+++ web/js/autocomplete.js
@@ -0,0 +1,8 @@
+import { app } from '../../../scripts/app.js';
+import { api } from '../../../scripts/api.js';
+
+app.registerExtension({
+  name: 'ComfyUI-ScenarioPrompt',
+
+  async endpointtest() {},
+});
