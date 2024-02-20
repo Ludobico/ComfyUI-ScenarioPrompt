87642481bd5a1b03380785b8d7414597b14e6876
Ludobico <89598307+Ludobico@users.noreply.github.com>
2024-02-20 16:55:38 +0900

    del : file delete

--- web/js/autocomplete.js
+++
@@ -1,8 +0,0 @@
-import { app } from '../../../scripts/app.js';
-import { api } from '../../../scripts/api.js';
-
-app.registerExtension({
-  name: 'ComfyUI-ScenarioPrompt',
-
-  async endpointtest() {},
-});
