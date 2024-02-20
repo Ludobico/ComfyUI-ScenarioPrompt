c1f07387eca2d3cdeb1eb7de1af08619fe14aa8c
Ludobico <89598307+Ludobico@users.noreply.github.com>
2024-02-20 16:53:31 +0900

    test : web connection test

--- web/js/web.js
+++ web/js/web.js
@@ -2,9 +2,20 @@ import { app } from '../../../scripts/app.js';
 import { api } from '../../../scripts/api.js';
 
 app.registerExtension({
-  name: 'ComfyUI-ScenarioPrompt',
+  name: 'ComfyUI.ScenarioPrompt',
   async setup() {
-    console.log('asdasdasd');
+    // api
+    //   .fetchApi('/ludobico/autocomplete')
+    //   .then((res) => {
+    //     const reader = res.body.getReader();
+    //     return reader.read();
+    //   })
+    //   .then(({ done, value }) => {
+    //     if (!done) {
+    //       const decoder = new TextDecoder('utf-8');
+    //       const decodedString = decoder.decode(value);
+    //       console.log(decodedString);
+    //     }
+    //   });
   },
-  async endpointtest() {},
 });
