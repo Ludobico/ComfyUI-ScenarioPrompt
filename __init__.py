145f71eda49705d36dc54e5e556e5a263810719d
Ludobico <89598307+Ludobico@users.noreply.github.com>
2024-02-21 10:56:39 +0900

    init : init

--- __init__.py
+++ __init__.py
@@ -7,10 +7,6 @@ from .s_prompt import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 cur_dir = os.getcwd()
 server_path = os.path.join(cur_dir, 'ComfyUI', 'server.py')
 
-@PromptServer.instance.routes.get("/ludobico/autocomplete")
-async def get_autocomplete(request):
-  return web.json_response("hello")
-
 WEB_DIRECTORY = './web'
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS', 'WEB_DIRECTORY']
 
